5b2a207efa252888a868944c33bff09418dc8554
WilliamLu <luen1@kingnet.com>
2018-12-20 16:22:27 +0800

    集成bugly及热更新

--- app/src/main/java/com/williamlu/gankio/GankIoApplation.kt
+++ app/src/main/java/com/williamlu/gankio/GankIoApplation.kt
@@ -1,6 +1,7 @@
 package com.williamlu.gankio
 
 import android.app.Application
+import android.content.Context
 import android.content.IntentFilter
 import android.net.ConnectivityManager
 import android.support.multidex.MultiDex
@@ -8,6 +9,7 @@ import com.orhanobut.logger.AndroidLogAdapter
 import com.orhanobut.logger.Logger
 import com.squareup.leakcanary.LeakCanary
 import com.tencent.bugly.Bugly
+import com.tencent.bugly.beta.Beta
 import com.williamlu.toolslib.GlobalCache
 import com.williamlu.toolslib.NetworkChangedReceiver
 import com.williamlu.toolslib.ToastUtils
@@ -41,6 +43,14 @@ class GankIoApplation : Application() {
         // 这里实现SDK初始化，appId替换成你的在Bugly平台申请的appId
         // 调试时，将第三个参数改为true
         Bugly.init(this, AppConstant.ConfigConstant.BUGLY_APPID, BuildConfig.DEBUG)
+    }
+
+    override fun attachBaseContext(base: Context) {
+        super.attachBaseContext(base)
+        // you must install multiDex whatever tinker is installed!
+        MultiDex.install(base)
+        // 安装tinker
+        Beta.installTinker()
     }
 
     private fun initLeakCanary() {
