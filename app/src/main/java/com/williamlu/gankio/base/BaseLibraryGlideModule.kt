c2d77594d3b48d32cf304b5ab21b4d4b306153cd
WilliamLu <luen1@kingnet.com>
2018-12-21 15:18:27 +0800

    Glide 4.8的使用

---
+++ app/src/main/java/com/williamlu/gankio/base/BaseLibraryGlideModule.kt
@@ -0,0 +1,13 @@
+package com.williamlu.gankio.base
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.LibraryGlideModule
+
+/**
+ * @Author: WilliamLu
+ * @Data: 2018/12/21
+ * @Description:
+ */
+@GlideModule
+class BaseLibraryGlideModule :LibraryGlideModule(){
+}
--- app/src/main/java/com/williamlu/gankio/home/adapter/MainListAdapter.kt
+++ app/src/main/java/com/williamlu/gankio/home/adapter/MainListAdapter.kt
@@ -1,9 +1,9 @@
 package com.williamlu.gankio.home.adapter
 
-import com.bumptech.glide.Glide
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.BaseViewHolder
 import com.williamlu.gankio.R
+import com.williamlu.gankio.base.GlideApp
 import com.williamlu.gankio.model.Movie
 
 /**
@@ -13,7 +13,7 @@ import com.williamlu.gankio.model.Movie
  */
 class MainListAdapter(data: List<Movie>) : BaseQuickAdapter<Movie, BaseViewHolder>(R.layout.item_main_movie, data) {
     override fun convert(helper: BaseViewHolder?, item: Movie?) {
-        Glide.with(mContext).load(item!!.images.large).into(helper!!.getView(R.id.item_main_iv))
+        GlideApp.with(mContext).load(item!!.images.large).into(helper!!.getView(R.id.item_main_iv))
         helper.setText(R.id.item_main_tv_name, item.title)
         helper.setText(R.id.item_main_tv_score, item.rating.average.toString())
     }
